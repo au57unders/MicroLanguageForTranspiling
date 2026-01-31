b7cfb27a811d36b11e54d7a0f6cf3ef8f8c9b980
orangePrint("ark") <orangeprint908@gmail.com>
2026-01-31 10:30:18 +0100

    added the sample files with their Exports

---
+++ sample.cs
@@ -0,0 +1,24 @@
+	
+	Console.WriteLine(150000);
+	100 + 500;
+	Console.WriteLine(10);
+	20 * 5;
+	Console.WriteLine(999);
+	double score = 100;
+	double bonus = 50;
+	double total = score + bonus;
+	if (total > 100) {
+
+	Console.WriteLine("HighScore");
+	Console.WriteLine(name);
+	} else if (total == 100) {
+
+	Console.WriteLine("100, interesting");
+	} else {
+
+	Console.WriteLine("loser");
+	}
+
+	total * 2;
+}
+}
---
+++ sample.rs
@@ -0,0 +1,31 @@
+fn main() {
+	
+	println!("{}", 150000);
+
+	100 + 500;
+	println!("{}", 10);
+
+	20 * 5;
+	println!("{}", 999);
+
+	let score = 100;
+	let bonus = 50;
+	let total = score + bonus;
+	if total > 100 {
+
+	println!("{}", "HighScore");
+
+	println!("{}", name);
+
+	} else if total == 100 {
+
+	println!("{}", "100, interesting");
+
+	} else {
+
+	println!("{}", "loser");
+
+	}
+
+	total * 2;
+}
